a2eabd192c4798f8a9790748a427d50f5cf26a32
Urs <Urs@White-Elephant.ch>
2025-09-24 16:47:39 +0200

    Ada_Server v45.2/0-18 - Use improved Os.Pipe

--- Ada/Servers/Ada_95/Ada_Server/Ada_Server.adb
+++ Ada/Servers/Ada_95/Ada_Server/Ada_Server.adb
@@ -5,7 +5,7 @@
 pragma Style_White_Elephant;
 
 pragma Build (Description => "Ada Server for NP++ Elephant Plugin",
-              Version     => (45, 2, 0, 17),
+              Version     => (45, 2, 0, 18),
               Kind        => Windows,
               Compiler    => "GNATPRO\23.0");
 
